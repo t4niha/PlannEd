cd27af18d91cdd4ebc2175ca9f7c512715e1174f
t4niha <tanihast@gmail.com>
2026-03-17 17:30:43 +0600

    Priority Withstanding

--- app/src/main/java/com/planned/MainActivity.kt
+++ app/src/main/java/com/planned/MainActivity.kt
@@ -51,7 +51,7 @@ class MainActivity : ComponentActivity() {
 }
 
 /**
- TODO: [-] Fix UI pages (category, event, deadline, reminder, task bucket) - all/this (isException)
+ TODO: [-] Fix UI pages (category, event, deadline, reminder, task bucket)
  TODO: [-] Calendar to UI pages navigation
  TODO: [ ] On creation don't allow duplicate titles for events, categories
  TODO: [ ] refresh schedule button
@@ -67,7 +67,6 @@ class MainActivity : ComponentActivity() {
  TODO: [-] Task update form dropdown loop glitch
  TODO: [-] Cancel (dialogue) buttons do same as Save?
  TODO: [-] Task form update needed after Save for dependency option
- TODO: [ ] UI Pages + isException logic
  TODO: [ ] Calendar to object pages
  TODO: [ ] Warning for insufficient bucket space
  TODO: [ ] What to do when impossible to meet deadline given current schedule order?
