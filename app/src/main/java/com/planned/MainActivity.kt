444944f31fae97cd5d73a8353a289937ad6b1750
t4niha <tanihast@gmail.com>
2026-03-22 02:22:51 +0600

    Update MainActivity.kt

--- app/src/main/java/com/planned/MainActivity.kt
+++ app/src/main/java/com/planned/MainActivity.kt
@@ -52,8 +52,8 @@ class MainActivity : ComponentActivity() {
 
 /**
 [-] DB Sample ATI
-[ ] Developer cards to separate tables, ATI Visualization, Task schedule order viewing
-[ ] Extend sample another future week
+[-] Developer cards to separate tables, ATI Visualization, Task schedule order viewing
+[-] Extend sample another future week
 [ ] Task fetch order alphabetically, but ID needed to get last for creation success message
 [?] On creation don't allow duplicate titles for events, categories?
 [?] Warning when tasks scheduled after deadline / impossible to meet deadline?
